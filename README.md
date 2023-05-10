de727ae2bebf5ff65c1b27b448748bd6b9ebbbb8
douglasacost <douglasacostaaquino@gmail.com>
2023-05-09 20:27:40 -0400

    readme updated

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 - run `yarn intall`
 - then `yarn start` to run the index.ts
 
+## Arguments
+
+> `yarn start [network] [maxBatchSize] [batchInterval]`
+
+- network: `testnet` | `mainnet` - default: `testnet`
+- maxBatchSize: (int) - default: `1`
+- batchInterval: (ms) - default: `1000`
+
 [...]
--- index.ts
+++ index.ts
@@ -29,26 +29,26 @@ class BatchJsonRpcClient {
     maxLogLevel: 9999999,
     cpuRateLimit: 0.5,
     logCallback: (level, target, message) => {
-        if (level > 3)
-            return;
-        // The first parameter of the methods of `console` has some printf-like substitution
-        // capabilities. We don't really need to use this, but not using it means that the logs
-        // might not get printed correctly if they contain `%`.
-        if (level <= 1) {
-            console.error("[%s] %s", target, message);
-        }
-        else if (level === 2) {
-            console.warn("[%s] %s", target, message);
-        }
-        else if (level === 3) {
-            console.info("[%s] %s", target, message);
-        }
-        else if (level === 4) {
-            console.debug("[%s] %s", target, message);
-        }
-        else {
-            console.trace("[%s] %s", target, message);
-        }
+      if (level > 3)
+        return;
+      // The first parameter of the methods of `console` has some printf-like substitution
+      // capabilities. We don't really need to use this, but not using it means that the logs
+      // might not get printed correctly if they contain `%`.
+      if (level <= 1) {
+        console.error("[%s] %s", target, message);
+      }
+      else if (level === 2) {
+        console.warn("[%s] %s", target, message);
+      }
+      else if (level === 3) {
+        console.info("[%s] %s", target, message);
+      }
+      else if (level === 4) {
+        console.debug("[%s] %s", target, message);
+      }
+      else {
+        console.trace("[%s] %s", target, message);
+      }
     },
   });
   private chain: smoldot.Chain | undefined;
@@ -56,7 +56,7 @@ class BatchJsonRpcClient {
   constructor(options: BatchJsonRpcOptions = {}) {
     (async () => {
       if (options.testnet) {
-        
+
         const rococo_ = this.chain = await this.client.addChain({
           chainSpec: JSON.stringify(rococo)
         });
@@ -114,16 +114,16 @@ class BatchJsonRpcClient {
     setInterval(async () => {
 
       this.chain!.nextJsonRpcResponse().then((response) => {
-          console.log('response:: ', response);
-          const parsed = JSON.parse(response);
-          const request = this.sentRequests.find((request) => {
-            return request.id === parsed.id;
-          });
-
-          if (request?.callback) {
-            request.callback(response);
-          }
+        console.log('response:: ', response);
+        const parsed = JSON.parse(response);
+        const request = this.sentRequests.find((request) => {
+          return request.id === parsed.id;
         });
+
+        if (request?.callback) {
+          request.callback(response);
+        }
+      });
     }, 1000);
 
   }
@@ -138,11 +138,11 @@ class BatchJsonRpcClient {
       const request = this.batchQueue[i];
       if (request) {
         try {
-            console.log('sending:: ', JSON.stringify(request))
-        this.chain!.sendJsonRpc(JSON.stringify(request));
-        this.sentRequests.push(request);
+          console.log('sending:: ', JSON.stringify(request))
+          this.chain!.sendJsonRpc(JSON.stringify(request));
+          this.sentRequests.push(request);
         } catch (error) {
-            console.log('error', error);
+          console.log('error', error);
         }
       }
     }
@@ -161,16 +161,19 @@ class BatchJsonRpcClient {
 
 const hex = '0x45028400c6f5bc3043731278e954cd2bf0456a0b7e5b28121af089fdc8353c8fe751255f01d49f8980adf580fd40735da3a1d0b19fed364a3e6265c4f2f9872b11f473ce4db163780448e6097a4e2be71bf0220748309cc635488d0ac2dc8af33c067cf286e5015109000203000e33071336d2a43c94668532a3929cdd3f8c6ac2ee1996325ca3ac9414583e2d0700e8764817'
 
-async function run () {
-  const client = new BatchJsonRpcClient({ testnet: false, maxBatchSize: 1, batchInterval: 1000 });
+async function run() {
+  const testnet = process.argv[2] === 'testnet';
+  const maxBatchSize = process.argv[3] ? parseInt(process.argv[3]) : 1;
+  const batchInterval = process.argv[4] ? parseInt(process.argv[4]) : 1000;
+  const client = new BatchJsonRpcClient({ testnet, maxBatchSize, batchInterval });
   await new Promise((resolve) => setTimeout(resolve, 4000));
 
   client.call('system_health', [], (response: string) => {
-    console.log('response:: ', response);
+    console.log('response system_health:: ', response);
   });
 
   client.call('author_submitAndWatchExtrinsic', [hex], (response: string) => {
-    console.log('response:: ', response);
+    console.log('response author_submitAndWatchExtrinsic:: ', response);
   });
 }
 
